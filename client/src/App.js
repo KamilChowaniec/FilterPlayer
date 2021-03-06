1a48b960946f8206de65a1254cb49e47cdafc295
Kamil Chowaniec <kamilchowaniec97@gmail.com>
2021-03-06 16:42:15 +0100

    Added base Url

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Room from "./views/Room/Room.jsx";
 import Home from "./views/Home/Home.jsx";
 
--- client/src/views/Room/Room.jsx
+++ client/src/views/Room/Room.jsx
@@ -1,3 +1,18 @@
-import React from 'react';
-const Room = () => <h1>room</h1>
+import React from "react";
+import Navbar from "components/Navbar/Navbar.jsx";
+import Playlist from "components/Playlist/Playlist.jsx";
+import Videoplayer from "components/Videoplayer/Videoplayer.jsx";
+import Chat from "components/Chat/Chat.jsx";
+
+const Room = () => (
+  <div className="container">
+    <Navbar />
+    <div className="">
+      <Playlist />
+      <Videoplayer />
+      <Chat />
+    </div>
+  </div>
+);
+
 export default Room;
