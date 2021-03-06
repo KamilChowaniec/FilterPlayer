e3eeee6c9efe03b1050fc827e2889e45f3b3812a
ers17 <ers17@aber.ac.uk>
2021-03-06 15:16:17 +0100

    first commit

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Chat = () => <></>
+export default Chat;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const Navbar = () => <nav className="navbar">
+    
+     </nav> 
+
+export default Navbar;
